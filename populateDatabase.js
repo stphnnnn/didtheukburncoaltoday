26500cee321bfdb11a773fb55c6ca3aac9204218
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-06-18 13:33:57 +0100

    update populate function

--- populateDatabase.js
+++ populateDatabase.js
@@ -7,21 +7,22 @@ exports = async function () {
     .db("coal")
     .collection("timeblocks");
 
-  const response = await context.http.get({
-    url: "https://api.carbonintensity.org.uk/generation",
-  });
+  const baseUrl = "https://drax-production.herokuapp.com/api/1/generation-mix";
 
+  // Get data from 1 hour ago to now
+  const to = new Date();
+  const from = new Date(to.getTime() - 60 * 60000);
+
+  const url = `${baseUrl}?group_by=30m&date_from=${from.toISOString()}&date_to=${to.toISOString()}`;
+
+  const response = await context.http.get({ url });
   const json = EJSON.parse(response.body.text());
 
-  // Transforms "generationmix" array into an object
-  const mix = json.data.generationmix.reduce((acc, curr) => {
-    acc[curr.fuel] = curr.perc;
-    return acc;
-  }, {});
+  const document = {
+    to: json[0].start,
+    from: json[0].end,
+    mix: json[0].value,
+  };
 
-  collection.insertOne({
-    to: json.data.to,
-    from: json.data.from,
-    mix: mix,
-  });
+  collection.insertOne(document);
 };
