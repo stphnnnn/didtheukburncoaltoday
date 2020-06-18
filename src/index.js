68389b1437733b508c72a75d89a66b6d155d3755
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-06-18 17:00:53 +0100

    styling and sharing updates

--- src/index.js
+++ src/index.js
@@ -9,25 +9,35 @@ async function init() {
   const response = await fetch(`/api`);
   const json = await response.json();
 
+  // Hide spinner
+  const spinner = document.querySelector(".spinner");
+  spinner.style.display = "none";
+
   // Did the UK burn coal today?
   const hasBurnedCoal = isSameDay(new Date(), parseISO(json.to));
 
   if (hasBurnedCoal) {
     document.getElementById("headline").innerHTML = `
-      <h1>Yes</h1>
+      <h1 class="red">Yes</h1>
     `;
   } else {
     document.getElementById("headline").innerHTML = `
-      <h1>No</h1>
+      <h1 class="blue">No</h1>
     `;
   }
 
   // No coal streak
   const streakInDays = differenceInDays(new Date(), parseISO(json.to));
 
-  document.getElementById("streak").innerHTML = `
-    It has been ${streakInDays} days since the UK last burned coal.
-  `;
+  if (hasBurnedCoal) {
+    document.getElementById("streak").innerHTML = `
+      But we don’t have to! Earlier this year, <a class="blue" href="https://www.theguardian.com/business/2020/apr/28/britain-breaks-record-for-coal-free-power-generation">the UK went months in a row</a> without burning coal.
+    `;
+  } else {
+    document.getElementById("streak").innerHTML = `
+      It has been <span class="blue">${streakInDays} days</span> since the UK last burned coal.
+    `;
+  }
 }
 
 init();
